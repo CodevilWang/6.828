84b8787871a3e27743cd9f78d75a2958c3554801
CodevilWang <codevil.w.m@gmail.com>
2019-05-24 15:58:28 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# used for init env on clean Ubuntu Server
+sudo apt-get update
+sudo apt-get install qemu
+sudo apt-get install make
+sudo apt-get install gcc
+sudo apt-get install g++
+sudo apt-get install build-essential
+sudo apt-get install gcc-5.4-multilib
+sudo apt-get install gcc-multilib
+sudo apt install python3-pip
+sudo apt install python
+pip3 install https://github.com/shadowsocks/shadowsocks/archive/master.zip
+sudo printf "{
+    \"server\":\"172.31.7.30\",
+    \"server_port\":8388,
+    \"local_address\":\"127.0.0.1\",
+    \"local_port\":1080,
+    \"password\":\"123456\",
+    \"timeout\":300,
+    \"method\":\"aes-256-cfb\"
+}" >/etc/shadowsocks.json
+sudo /home/ubuntu/.local/bin/ssserver -c /etc/shadowsocks.json -d start
